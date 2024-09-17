a417a1112eebe94130494e94f5a3a1ab28a4c778
Nischhelm <nischhelm@gmail.com>
2024-09-17 12:03:45 +0200

    Updated Version

--- src/main/java/srpmultiplier/SRPMultiplier.java
+++ src/main/java/srpmultiplier/SRPMultiplier.java
@@ -12,7 +12,7 @@ import srpmultiplier.handlers.SRPSpawningHandler;
 @Mod(modid = SRPMultiplier.MODID, version = SRPMultiplier.VERSION, name = SRPMultiplier.NAME, dependencies = "required-after:fermiumbooter")
 public class SRPMultiplier {
     public static final String MODID = "srpmultiplier";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String NAME = "SRPMultiplier";
     public static final Logger LOGGER = LogManager.getLogger();
 
