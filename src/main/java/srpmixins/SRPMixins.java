7b885939f088261553e242544216c682057a55af
nischhelm <nischhelm@gmail.com>
2026-07-20 21:07:06 +0200

    v2.9.6.1
    Configurable adaptation effectiveness and fire damage multiplier when wearing liv/sent armor and burning. Effect by default reduced by 50%, so it's less awful to the player now.

--- src/main/java/srpmixins/SRPMixins.java
+++ src/main/java/srpmixins/SRPMixins.java
@@ -48,7 +48,7 @@ import srpmixins.world.SRPWorldProvider;
 )
 public class SRPMixins {
     public static final String MODID = "srpmixins";
-    public static final String VERSION = "2.9.6";
+    public static final String VERSION = "2.9.6.1";
     public static final String NAME = "SRPMixins";
     public static final Logger LOGGER = LogManager.getLogger();
     public static Configuration CONFIG;
